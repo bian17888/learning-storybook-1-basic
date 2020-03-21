d8904ca0b3d77fafef96afe21185237f6dc8563d
zhuzhen.bk <zhuzhen.bk@alibaba-inc.com>
2020-03-21 20:20:51 +0800

    add theme

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,14 +1,29 @@
-import { configure } from '@storybook/react';
+import { configure, addParameters, addDecorator } from '@storybook/react';
+import { withInfo } from '@storybook/addon-info'
 
 import '../bootstrap-reboot.min.css';
 import '../bootstrap.css';
 import '../bootstrap-grid.css';
 import '../main.css';
 
+import crfTheme from './crfTheme'
+
 // automatically import all files ending in *.stories.js
 const req = require.context('../stories', true, /\.stories\.js$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
+
+// 
+addParameters({
+  options: {
+    theme: crfTheme
+  }
+})
+
+// config withInfo
+addDecorator(withInfo({
+  inline: false
+}));
 
 configure(loadStories, module);
