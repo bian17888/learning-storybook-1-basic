76aac7edf425d43887e77c6fe9a66e6788df032b
zhuzhen.bk <zhuzhen.bk@alibaba-inc.com>
2020-03-26 21:37:42 +0800

    create a user's panel

--- stories/banner.stories.js
+++ stories/banner.stories.js
@@ -1,11 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { storiesOf } from '@storybook/react'
 import { text } from '@storybook/addon-knobs'
 
 import MajorBanner from '../components/major.banner'
 import MinorBanner from '../components/minor.banner'
+import withSeeAlso from '../.storybook/panel/SeeAlso/index'
 
 storiesOf('Component | Banner/Major', module)
+    .addDecorator(withSeeAlso)
+    .addParameters({
+        seeAlso: { story: 'Component | Banner/Minor', label: 'Beck - Minor Banners hh' },
+    })
     .add('with only title', () => {
         const title = text('Title', 'title')
         const photo = text('Photo', 'People Outdoors/shutterstock_116403520.jpg')
